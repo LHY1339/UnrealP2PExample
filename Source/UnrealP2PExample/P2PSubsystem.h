b67cb052f14a696c7f1877bb22376ebe9190670f
LHY1339 <123950909+LHY1339@users.noreply.github.com>
2025-06-09 21:29:21 +0800

    update

---
+++ Source/UnrealP2PExample/P2PSubsystem.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "P2PSubsystem.generated.h"
+
+
+UCLASS()
+class UNREALP2PEXAMPLE_API UP2PSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+};
